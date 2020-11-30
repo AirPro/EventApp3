2c15cd29506eed9b5f98a47e205f8ccad75fb873
Bob Freid <freidrd@mail.uc.edu>
2020-11-29 20:50:51 -0500

    Working on adding a new event to the database

---
+++ Controllers/EventController.cs
@@ -0,0 +1,40 @@
+﻿using EventApp3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventApp3.Controllers
+{
+	public class EventController : Controller
+	{
+		private FreidrdFinalProjectContext context { get; set; }
+		public EventController(FreidrdFinalProjectContext ctx)
+		{
+			context = ctx;
+		}
+
+		[HttpGet]
+		public IActionResult Add()
+		{
+			ViewBag.Action = "Add";
+			ViewBag.Events = context.Events.OrderBy(e => e.EventId).ToList();
+			return View("Edit", new Event());
+		}
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			ViewBag.Action = "Edit";
+			ViewBag.Events = context.Events.OrderBy(e => e.EventId).ToList();
+			var event = context.Events.Find(id);
+            return View(event);
+		}
+
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
+	}
+}
