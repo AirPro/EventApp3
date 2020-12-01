b30769577373a688472fe65c6b5eab4c0812d41d
Bob Freid <freidrd@mail.uc.edu>
2020-12-01 11:29:00 -0500

    Added the Migrations folder for the creating of the database off the fly for the instructor or grader.

---
+++ Migrations/20201201162712_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventApp3.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EventCity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EventDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VenueName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VenueAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PromoterName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
