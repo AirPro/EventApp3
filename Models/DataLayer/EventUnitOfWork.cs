5c2efb45f9e6a2cd47599bfa82aba632c3a18fc9
Bob Freid <freidrd@mail.uc.edu>
2020-12-03 17:46:09 -0500

    Added EventUnitOfWork.cs class

---
+++ Models/DataLayer/EventUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using EventApp3.Models.DomainModels;
+using System;
+
+namespace EventApp3.Models.DataLayer
+{
+	public class EventUnitOfWork : IEventUnitOfWork
+	{
+		private FreidrdFinalProjectContext context { get; set; }
+		public EventUnitOfWork(FreidrdFinalProjectContext ctx) => context = ctx;
+
+        private Repository<Event> dayData;
+        public Repository<Event> Days
+        {
+            get
+            {
+                if (dayData == null)
+                    dayData = new Repository<Event>(context);
+                return dayData;
+            }
+        }
+
+		public Repository<Event> Events => throw new NotImplementedException();
+
+		public void Save() => context.SaveChanges();
+    }
+
+}
